6fb170b67fbbd6f039ad57a96acacc22b7b47a40
ViktoriaBy <v.chamborant@hotmail.com>
2022-07-27 12:30:38 -0500

    started on the blog

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login")
+def login():
+    return "Login"
+
+@auth.route("/sign-up")
+def sign_up():
+    return "Sign up"
+
+@auth.route("/sign-out")
+def sign_out():
+    return "Sign out"
