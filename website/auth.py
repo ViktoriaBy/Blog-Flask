6dfd3e84eb168bd38d0ce49f374256bc43576e3c
ViktoriaBy <v.chamborant@hotmail.com>
2022-08-15 15:04:43 -0500

    added flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
@@ -18,6 +18,11 @@ def login():
                 flash('Logged in!', category='message')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect!', category='error')
+        else:
+            flash('Email does not exist!', category='error')
+
 
     return render_template("login.html")
 
@@ -31,6 +36,7 @@ def sign_up():
 
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
+
         if email_exists:
             flash('Email already in use!', category='error')
         elif username_exists:
@@ -44,14 +50,18 @@ def sign_up():
         elif len(email) < 4:
             flash('Email is invalid!', category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sh256'))
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
 
+
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
