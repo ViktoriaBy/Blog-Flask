02365e2b6ad7a2ce453be06bbb0440ed4dee1889
ViktoriaBy <v.chamborant@hotmail.com>
2022-08-15 14:27:00 -0500

    login page created

--- website/auth.py
+++ website/auth.py
@@ -8,8 +8,17 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!', category='message')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+
     return render_template("login.html")
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
@@ -35,7 +44,7 @@ def sign_up():
         elif len(email) < 4:
             flash('Email is invalid!', category='error')
         else:
-            new_user = User(email=email, username=username, password=password1)
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sh256'))
             db.session.add(new_user)
             db.session.commit()
             flash('User created!')
